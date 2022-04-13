521e1ce3a5fe1c36c32cfc98f808c7c5c2ded5bd
Matheus Camba <mhscamba@gmail.com>
2022-04-13 12:36:14 -0300

    modificacao App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button/Button";
+import Card from "./components/Cards/Cards";
+import Label from "./components/Label/Label";
+import style from "./input.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [produtos, setProdutos] = useState([]);
+
+  const [busca, setBusca] = useState();
+
+  const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=${busca}&source=nanook`
+      )
+      .then((resposta) => {
+        setProdutos(resposta.data.products);
+        console.log(resposta.data);
+        console.log(resposta.data.products[0]._meta.score);
+      });
+  }, [update]);
+
+  function handlebusca(e) {
+    let value = e.target.value;
+    setBusca(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="div">
+        <Label />
+        <input
+          type="text"
+          placeholder="Sugestão: camisa, iphone, maquina de lavar..."
+          className={style.input}
+          value={busca}
+          onChange={handlebusca}
+        />
+      </div>
+      <div className="div">
+        <Button onClick={() => setUpdate(!update)} />
+      </div>
+      <div className="produtos">
+        <Card produtos={produtos} />
+      </div>
+    </>
   );
 }
 
